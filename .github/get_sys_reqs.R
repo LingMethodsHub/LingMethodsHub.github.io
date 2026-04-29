68fc1d72cf577a412450e8819e775f6241a98007
JoFrhwld <JoFrhwld@gmail.com>
2026-04-29 14:39:12 -0400

    sysreqs script

---
+++ .github/get_sys_reqs.R
@@ -0,0 +1,11 @@
+library(getsysreqs)
+
+reqs <- get_sysreqs(
+   "renv.lock",
+   distribution = "ubuntu",
+   release = "20.04"
+)
+
+libs <- paste(reqs, collapse = " ")
+cmd <- paste("sudo apt-get -y --no-install-recommends install", libs)
+cat(cmd)
